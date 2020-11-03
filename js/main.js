c37d670ee497ac025b215104236339f75949ba4d
Eve <evaforgacs@hotmail.com>
2020-11-03 20:27:49 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-const form = document.querySelector('.form-div form');
-form.addEventListener('submit', (ev) => {
-    //ev.preventDefault();
+// const form = document.querySelector('.form-div form');
+// form.addEventListener('submit', (ev) => {
+//     //ev.preventDefault();
     
-    const name = document.querySelector('input[name=name]');
-    const email = document.querySelector('input[name=email]');
-    const message = document.querySelector('textarea[name=message]');
+//     const name = document.querySelector('input[name=name]');
+//     const email = document.querySelector('input[name=email]');
+//     const message = document.querySelector('textarea[name=message]');
 
-console.log(name, email, message);
+// console.log(name, email, message);
 
-    if ( parseInt(name.getAttribute('minlength')) > name['value'].length ) {
-        alert('A Név mező tartalma nem megfelelő.');
-        ev.preventDefault();
+//     if ( parseInt(name.getAttribute('minlength')) > name['value'].length ) {
+//         alert('A Név mező tartalma nem megfelelő.');
+//         ev.preventDefault();
     
-});
+// });
